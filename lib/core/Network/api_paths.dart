ee755a7f2b661466fcd2a0dc51393c03a0011655
Phharoah <eng.ahmed7gazy@gmail.com>
2025-05-18 18:15:35 +0300

    18/5

--- lib/core/Network/api_paths.dart
+++ lib/core/Network/api_paths.dart
@@ -3,4 +3,7 @@ abstract class ApiPaths {
       'https://ntitodo-production-676c.up.railway.app/api/';
   static const String register = 'register';
   static const String login = 'login';
+   static const String getUserData = 'get_user_data';
+  static const String addTask = 'new_task';
+  static const String refreshToken = 'refresh_token';
 }
